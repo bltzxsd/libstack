ae80928aa49038f42d672453b5ef70fb0edab543
bltzxsd <blissardartinq@gmail.com>
2023-06-15 00:42:33 +0530

    better err msg

--- src/db.rs
+++ src/db.rs
@@ -3,10 +3,22 @@ use std::env;
 use diesel::{Connection, PgConnection};
 use dotenvy::dotenv;
 
+// #[derive(Debug, Serialize)]
+// pub struct Response<'r> {
+//     status: &'r str,
+//     msg: &'r str,
+// }
+
+// impl<'r> Response<'r> {
+//     pub fn new(status: &'r str, msg: &'r str) -> Self {
+//         Self { status, msg }
+//     }
+// }
+
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
     PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+        .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
 }
