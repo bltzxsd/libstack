ff0808a335b14ea24402e3059fc3340f04724950
bltzxsd <blissardartinq@gmail.com>
2023-06-15 00:43:37 +0530

    book handlers

--- src/books/handlers.rs
+++ src/books/handlers.rs
@@ -1,31 +1,20 @@
-use crate::books::models::update_book;
-use crate::db::establish_connection;
-use actix_web::delete;
-use actix_web::get;
-use actix_web::put;
-use actix_web::web;
-use actix_web::Responder;
-use actix_web::{ post, HttpResponse };
+use crate::{books::models::update_book, db::establish_connection};
+use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 
-use super::models::add_book as create_book;
-use super::models::NewBook;
-use super::models::delete_book;
-use super::models::get_book;
+use super::models::{add_book as create_book, delete_book, get_book, NewBook};
 
-#[post("/books")]
+#[post("/books/new")]
 async fn add_book(payload: web::Json<NewBook>) -> impl Responder {
     let mut connection = establish_connection();
     let isbn = &*payload.isbn.clone().unwrap_or("null".to_string());
-    match
-        create_book(
-            &payload.title,
-            &payload.author,
-            payload.publication_year,
-            isbn,
-            payload.availability_status,
-            &mut connection
-        )
-    {
+    match create_book(
+        &payload.title,
+        &payload.author,
+        payload.publication_year,
+        isbn,
+        payload.availability_status,
+        &mut connection,
+    ) {
         Ok(book_id) => HttpResponse::Ok().json(book_id),
         Err(e) => HttpResponse::InternalServerError().json(format!("{e}")),
     }
@@ -36,7 +25,7 @@ async fn remove_book(book_id: web::Path<uuid::Uuid>) -> impl Responder {
     let mut connection = establish_connection();
 
     match delete_book(*book_id, &mut connection) {
-        Ok(_) => HttpResponse::Ok().json(format!("Deleted {book_id}")),
+        Ok(_) => HttpResponse::Ok().json(format!("deleted {book_id}")),
         Err(e) => HttpResponse::NotFound().json(format!("{e}")),
     }
 }
@@ -47,7 +36,7 @@ async fn fetch_book(book_id: web::Path<uuid::Uuid>) -> impl Responder {
 
     match get_book(*book_id, &mut conn) {
         Ok(Some(book)) => HttpResponse::Ok().json(book),
-        Ok(None) => HttpResponse::NotFound().finish(),
+        Ok(None) => HttpResponse::NotFound().json(format!("book {book_id} not found")),
         Err(e) => HttpResponse::InternalServerError().json(format!("{e}")),
     }
 }
@@ -55,12 +44,16 @@ async fn fetch_book(book_id: web::Path<uuid::Uuid>) -> impl Responder {
 #[put("/books/{book_id}")]
 async fn change_book(
     book_id: web::Path<uuid::Uuid>,
-    book_request: web::Json<NewBook>
+    book_request: web::Json<NewBook>,
 ) -> impl Responder {
     let mut conn = establish_connection();
     match update_book(*book_id, book_request.into_inner(), &mut conn) {
         Ok(updated) => {
-            if updated { HttpResponse::Ok().finish() } else { HttpResponse::NotFound().finish() }
+            if updated {
+                HttpResponse::Ok().finish()
+            } else {
+                HttpResponse::NotFound().finish()
+            }
         }
         Err(e) => HttpResponse::InternalServerError().json(format!("{e}")),
     }
