b2d19757f03ebd28ae469710964fe78d61983b39
bltzxsd <blissardartinq@gmail.com>
2023-06-15 00:43:56 +0530

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
-#![warn(clippy::all, clippy::restriction, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![warn(clippy::correctness, clippy::suspicious, clippy::perf, clippy::style)]
 
-use actix_web::middleware;
-use actix_web::web::Data;
-use actix_web::App;
-use actix_web::HttpServer;
-use actix_web::Responder;
-use diesel::r2d2::ConnectionManager;
-use diesel::PgConnection;
+use actix_web::{middleware, web::Data, App, HttpServer, Responder};
+use diesel::{r2d2::ConnectionManager, PgConnection};
 
 mod books;
 mod db;
+mod errors;
+mod loans;
 mod members;
 mod schema;
 
 #[actix_web::get("/")]
 async fn hello() -> impl Responder {
-    actix_web::HttpResponse::Ok().json("hello from rust!")
+    actix_web::HttpResponse::Ok().json("ok")
 }
 
 #[actix_rt::main]
@@ -27,7 +24,9 @@ async fn main() -> std::io::Result<()> {
     // set up database config
     let database_url = std::env::var("DATABASE_URL").expect("Database url is not set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
-    let pool = r2d2::Pool::builder().build(manager).expect("Failed to create pool");
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
 
     // start server
     HttpServer::new(move || {
@@ -43,7 +42,11 @@ async fn main() -> std::io::Result<()> {
             .service(members::handlers::fetch_member)
             .service(members::handlers::change_member)
             .service(members::handlers::remove_member)
+            .service(loans::handlers::new_loan)
+            .service(loans::handlers::fetch_loan)
+            .service(loans::handlers::close_loan)
     })
-        .bind("127.0.0.1:9090")?
-        .run().await
+    .bind("127.0.0.1:9090")?
+    .run()
+    .await
 }
