9d4402e68b12c9e3908da24c87e673a927381158
bltzxsd <blissardartinq@gmail.com>
2023-06-15 00:41:45 +0530

    update loan migration

--- migrations/2023-05-29-103109_loans/down.sql
+++ migrations/2023-05-29-103109_loans/down.sql
@@ -1,3 +1,5 @@
 -- This file should undo anything in `up.sql`
 
 DROP TABLE loans;
+
+DROP TYPE IF EXISTS loan_status;
--- migrations/2023-05-29-103109_loans/up.sql
+++ migrations/2023-05-29-103109_loans/up.sql
@@ -2,6 +2,8 @@
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Enable UUID extension
 
+CREATE TYPE loan_status AS ENUM ('open', 'returned', 'overdue');
+
 CREATE TABLE loans (
     loan_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     member_id UUID NOT NULL,
@@ -9,7 +11,7 @@ CREATE TABLE loans (
     loan_date DATE NOT NULL,
     due_date DATE NOT NULL,
     return_date DATE,
-    fine INT DEFAULT 0,
+    status loan_status NOT NULL,
     FOREIGN KEY (member_id) REFERENCES members (member_id),
     FOREIGN KEY (book_id) REFERENCES books (book_id)
 );
